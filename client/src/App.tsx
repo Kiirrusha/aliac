eb5156339539d231d77b08a3f92fc7122318ac8f
Ruslick <motsukov@gmail.com>
2024-05-12 21:41:53 +0300

    front update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,15 @@
 import { RouterProvider } from "react-router-dom";
 import { router } from "src/pages/router";
+import * as SignalR  from "@microsoft/signalr";
+
+const connection = new SignalR.HubConnectionBuilder()
+  .withUrl("ws://localhost:3000/room")
+  .build();
+  
+connection.on("UpdateRoom", (data) => {
+  console.log(data);
+});
+
 
 const App = () => {
   return <RouterProvider router={router} />;
